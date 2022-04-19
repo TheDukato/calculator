ee5825a94a9d23e0ac8cc014661b61477325dfbd
TheDukato <kpokusa99@gmail.com>
2022-04-19 19:36:29 +0200

    First two tests working

--- src/com/Study_6/lab7/calc.java
+++ src/com/Study_6/lab7/calc.java
@@ -11,56 +11,93 @@ import java.awt.event.ActionEvent;
  */
 public class calc {
         public static JTextField jt;
-    public static void createAndShowGUI() {
-            //GUI
-            JFrame jf = new JFrame("My Calculator");
-            jf.setLocationRelativeTo(null);
-            jf.setResizable(false);
+        public static void createAndShowGUI() {
+                //GUI
+                JFrame jf = new JFrame("My Calculator");
+                jf.setLocationRelativeTo(null);
+                jf.setResizable(false);
 
-            jt = new JTextField("0");
-            jt.setEditable(false);
-            jt.setHorizontalAlignment(SwingConstants.RIGHT);
-            jf.getContentPane().add(jt,BorderLayout.NORTH);
+                jt = new JTextField("0");
+                jt.setEditable(false);
+                jt.setHorizontalAlignment(SwingConstants.RIGHT);
+                jf.getContentPane().add(jt,BorderLayout.NORTH);
 
-            JPanel jp = new JPanel();
-            jp.setLayout(new GridLayout(4,4));
-            jf.getContentPane().add(jp, BorderLayout.CENTER);
-            //Adding button
-            char[] signs = {'1','2','3','+','4','5','6','-','7','8','9','*','0','=','C','/'};
-            for(char tab : signs){
-                    JButton jb = new JButton(String.valueOf(tab));
-                    jb.addActionListener(new MyActionListener());
-                    jp.add(jb);
-            }
-            jf.pack();
-            jf.setVisible(true);
-            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+                JPanel jp = new JPanel();
+                jp.setLayout(new GridLayout(4,4));
+                jf.getContentPane().add(jp, BorderLayout.CENTER);
+                //Adding button
+                char[] signs = {'1','2','3','+','4','5','6','-','7','8','9','*','0','=','C','/'};
+                for(char tab : signs){
+                        JButton jb = new JButton(String.valueOf(tab));
+                        jb.addActionListener(new MyActionListener());
+                        jp.add(jb);
+                }
+                jf.pack();
+                jf.setVisible(true);
+                jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
 }
 class MyActionListener implements java.awt.event.ActionListener {
         static private char sign;
         static private int var1;
         static private int var2;
         static private boolean save2var1 = true;
-        public void MyActionListener(){
-                var1 = 0;
-                var2 = 0;
-                save2var1 = true;
-        }
+        static private boolean pocesed;
+        /*        public void MyActionListener(){
+                        var1 = 0;
+                        var2 = 0;
+                        save2var1 = true;
+                }*/
         public void actionPerformed(ActionEvent e) {
                 switch (e.getActionCommand().charAt(0)){
                         case '*':
+                                if(!pocesed){
+                                        switch (sign){
+                                                case '*':
+                                                        var1=var1*var2;
+                                                        break;
+                                                case '/':
+                                                        var1=var1/var2;
+                                                        break;
+                                                case '+':
+                                                        var1=var1+var2;
+                                                        break;
+                                                case '-':
+                                                        var1=var1-var2;
+                                                        break;
+                                        }
+                                        calc.jt.setText(String.valueOf(var1));
+                                }
                                 sign = '*';
                                 save2var1 = false;//Przejscie do kolejnej cyfry var2
                                 var2 = 0;
+                                pocesed=false;
                                 break;
                         case '/':
+                                if(!pocesed){
+                                        switch (sign){
+                                                case '*':
+                                                        var1=var1*var2;
+                                                        break;
+                                                case '/':
+                                                        var1=var1/var2;
+                                                        break;
+                                                case '+':
+                                                        var1=var1+var2;
+                                                        break;
+                                                case '-':
+                                                        var1=var1-var2;
+                                                        break;
+                                        }
+                                        calc.jt.setText(String.valueOf(var1));
+                                }
                                 sign = '/';
                                 save2var1 = false;//Przejscie do kolejnej cyfry var2
                                 var2 = 0;
+                                pocesed=false;
                                 break;
                         case '+':
-                                if(sign != '0'){
+                                if(!pocesed){
                                         switch (sign){
                                                 case '*':
                                                         var1=var1*var2;
@@ -80,11 +117,30 @@ class MyActionListener implements java.awt.event.ActionListener {
                                 sign = '+';
                                 save2var1 = false;//Przejscie do kolejnej cyfry var2
                                 var2 = 0;
+                                pocesed=false;
                                 break;
                         case '-':
+                                if(!pocesed){
+                                        switch (sign){
+                                                case '*':
+                                                        var1=var1*var2;
+                                                        break;
+                                                case '/':
+                                                        var1=var1/var2;
+                                                        break;
+                                                case '+':
+                                                        var1=var1+var2;
+                                                        break;
+                                                case '-':
+                                                        var1=var1-var2;
+                                                        break;
+                                        }
+                                        calc.jt.setText(String.valueOf(var1));
+                                }
                                 sign = '-';
                                 save2var1 = false;//Przejscie do kolejnej cyfry var2
                                 var2 = 0;
+                                pocesed=false;
                                 break;
                         case 'C':
                                 var1=0;
@@ -92,6 +148,7 @@ class MyActionListener implements java.awt.event.ActionListener {
                                 sign=0;
                                 save2var1 = true;
                                 calc.jt.setText("0");
+                                pocesed=true;
                                 break;
                         case '=':
                                 //System.out.print(String.valueOf(var1)+sign+String.valueOf(var2)+"=");
@@ -111,8 +168,9 @@ class MyActionListener implements java.awt.event.ActionListener {
                                 }
                                 //System.out.print(var1);
                                 calc.jt.setText(String.valueOf(var1));
+                                pocesed=true;
                                 //var2=0;
-                                sign=0;
+                                //sign=0;
                                 break;
                         default:
                                 //Entering multi digits number
@@ -132,16 +190,5 @@ class MyActionListener implements java.awt.event.ActionListener {
                                 }
                                 break;
                 }
-                /**
-                 * Mozliwe ze przyjmiemy:
-                 *      cyfra                                           GIT
-                 *      operator                                        GIT
-                 *      operator po operatorze                          GIT
-                 *      rowna sie po cyfrze                             GIT
-                 *      ###################################################
-                 *      operacje z dwoma znakami wywala się             GIT
-                 *      12+3-5 '=====' 15-5                             GIT
-                 *      jakie przypadki jeszcze?
-                 */
         }
 }
